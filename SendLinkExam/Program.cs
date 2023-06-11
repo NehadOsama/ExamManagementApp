caf5aa207fa0db177246d1ff5ff97ad3c045d05d
NehadOsama <NehadOsama39@gmail.com>
2023-06-11 18:41:41 +0300

    Add files via upload

---
+++ SendLinkExam/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+using static System.Net.WebRequestMethods;
+using System.Configuration;
+
+namespace SendLinkExam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:7253/ExamTaker/SendMailExamURL");
+
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+            const SslProtocols _Tls12 = (SslProtocols)0x00000C00;
+            const SecurityProtocolType Tls12 = (SecurityProtocolType)_Tls12;
+            ServicePointManager.SecurityProtocol = Tls12;
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)768;
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            string RootURL = ConfigurationManager.AppSettings["RootURL"];
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(RootURL + "/ExamTaker/SendMailExamURL");
+            request.Method = "GET";
+            //specify other request properties
+            Console.WriteLine("End");
+            try
+            {
+              var  response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
